a3da6c0506f6d8910163747af8f19fb17fa1dbe4
rianaditro <rianaditro@gmail.com>
2023-11-09 11:44:49 +0700

    ikea might use api, so here we are. STUCK.

--- ikea/scrap.py
+++ ikea/scrap.py
@@ -3,7 +3,6 @@ from requests_html import HTMLSession
 url = "https://www.beerwulf.com/en-gb/c/all-beers?segment=Beers&catalogCode=Beer_1"
 
 session = HTMLSession()
-
 r = session.get(url)
 
 print(r.status_code)
