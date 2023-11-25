41785b29aa9e5ef5f322b8b372e85ff1c3e59b2e
rianaditro <rianaditro@gmail.com>
2023-11-25 15:04:27 +0700

    migrating to linux: fix chromium, tested on link.py

--- ikea/link.py
+++ ikea/link.py
@@ -5,5 +5,8 @@ url = "https://www.ikea.co.id/in/produk/dekorasi/jam"
 session = HTMLSession()
 r = session.get(url)
 r.html.render(sleep=1)
-link = r.html.xpath('//*[@id="product-list-component"]/div[4]/div', first=True).absolute_links
-print(len(link))
+links = r.html.absolute_links
+#link = r.html.next(fetch=False)
+len = len(links)
+print(len)
+print(links)
